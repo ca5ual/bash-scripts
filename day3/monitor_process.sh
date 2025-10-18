78d3b1e6290289109693fbe5d3acb43ef50fd6c4
Ubuntu <ubuntu@ip-172-31-23-218.ec2.internal>
2025-10-18 13:03:37 +0000

    bug fix

--- day3/monitor_process.sh
+++ day3/monitor_process.sh
@@ -4,21 +4,20 @@ usage_info() {
   echo "Usage: $0 <process_name>"
 }
 
-# Check if process name is provided
-if [ $# -ne 1 ] then
-  echo "Error: Please provide a process name to monitor."
+# Check if process name is provided and valid
+if [ $# -eq 0 ]; then
+  echo "Error: Process name not found"
   usage_info
   exit 1
 fi
 
+process_name="$1"
 
 #check if the specified process is running
 process_check() {
-local process_name="$1"
 #if the process is found, pgrep returns pids, otherwise returns nothing
   if pgrep -x "${process_name}" > /dev/null; then
     echo "Process ${process_name} is running."
-    exit 0
   else
     echo "Process ${process_name} is not running."
     running_process
@@ -26,22 +25,27 @@ local process_name="$1"
 }
 
 
-restarting_process() {
+running_process() {
   echo "Attempting to start process ${process_name}..."
   local attempts=3
   #the script will attempt to restart the process automatically for three times
   for (( i=1; i<=attempts; i++ )); do
-    systemctl start "${process_name}"
+    sudo systemctl start "${process_name}"
     if [ $? -eq 0 ]; then
       echo "Process ${process_name} restarted successfully."
-      return
+      return 0
+    elif [ $? -ne 0 ]; then
+      echo "Error: process name not found."
+      exit 1
     else
       echo "Restart attempt $((i+1)) failed. Retrying..."
       sleep 5
-    fi 
+    fi
+  echo "Error: try to start process ${process_name} manually"
   done
 }
 
 
-
+#script logic
+process_check
 
